5590eb0d4db55269c3d7267384b6dd4c5d2e6adf
Raj Kundu <rjk368@gmail.com>
2022-09-24 16:14:06 -0400

    fix incorrect GL version on non-Apple devices

--- main.cpp
+++ main.cpp
@@ -50,7 +50,7 @@ int main(int argc, char* argv[])
   // GL 3.0 + GLSL 130
   const char* glsl_version = "#version 130";
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
   //glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
   //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // 3.0+ only
 #endif
